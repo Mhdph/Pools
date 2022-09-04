a3e35169f3b61c93f71f74e63d108712cc1586be
mahdi parivash <mahdiparivash@protonmail.com>
2022-09-04 15:23:13 +0430

    add tailwind css

---
+++ src/components/HomeHero.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HomeHero",
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
