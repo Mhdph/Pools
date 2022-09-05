c0af262d056d2ef1387e19c59eaee6aecca5b642
mahdi parivash <mahdiparivash@protonmail.com>
2022-09-05 13:22:31 +0430

    New Pool and testimonial complate

---
+++ src/components/Home/NewPool.vue
@@ -0,0 +1,178 @@
+<template lang="">
+  <hr class="w-[70%] border border-gray-500 border-opacity-5 my-4 ml-40" />
+
+  <div
+    class="flex bg-[#f6f7fe] justify-center font-dana overflow-scroll mb-20 w-full -mt-40 gap-4"
+  >
+    <div class="w-64 mr-[1100px] md:mr-[700px] lg:mr-80 2xl:mr-0 2xl:ml-10">
+      <p class="text-2xl mt-8 font-bold">
+        جدیدترین‌ <span class="text-[#1548FE]">استخـــرها</span>
+      </p>
+      <p class="text-[#454548] font-normal text-xs">
+        از جدیدترین استخر های تهران و شهرستان ها در لحظه با خبر شوید و با یک
+        کلیک بلیط را تهیه کنید ، آی پولز اینجاست جدیدترین استخر ها را به شما
+        معرفی کند.
+      </p>
+      <img
+        src="./../../assets/slideicon.svg"
+        class="h-20 w-20 mt-6 mr-40"
+        alt=""
+      />
+    </div>
+    <div
+      class="flex flex-col items-center bg-white p-4 h-full shadow-2xl rounded-xl"
+    >
+      <img
+        class="rounded-xl w-[264px] h-[187px] object-cover"
+        alt=""
+        src="../../assets/group-123@2x.png"
+      />
+      <div class="">
+        <b
+          class="inline-block text-lg text-[#454548] mt-4 [text-shadow:0px_0px_0px_rgba(255,_255,_255,_0.01)]"
+        >
+          استخر مریم قم
+        </b>
+      </div>
+    </div>
+    <div
+      class="flex flex-col items-center bg-white p-4 h-full shadow-2xl rounded-xl"
+    >
+      <img
+        class="rounded-xl w-64 h-[187px] object-cover"
+        alt=""
+        src="../../assets/group-12@2x.png"
+      />
+      <div class="">
+        <b
+          class="inline-block mt-4 text-lg text-[#454548] [text-shadow:0px_0px_0px_rgba(255,_255,_255,_0.01)]"
+          >استخر درین تهران</b
+        >
+      </div>
+    </div>
+    <div
+      class="flex flex-col items-center bg-white p-4 h-full shadow-2xl rounded-xl"
+    >
+      <img
+        class="rounded-xl w-[264px] h-[187px] object-cover"
+        alt=""
+        src="../../assets/group-121@2x.png"
+      />
+      <div class="">
+        <b
+          class="inline-block text-lg text-[#454548] mt-4 [text-shadow:0px_0px_0px_rgba(255,_255,_255,_0.01)]"
+        >
+          استخر خورشید مشهد
+        </b>
+      </div>
+    </div>
+    <div
+      class="flex flex-col items-center bg-white p-4 h-full shadow-2xl rounded-xl"
+    >
+      <img
+        class="rounded-xl w-[264px] h-[187px] object-cover"
+        alt=""
+        src="../../assets/group-122@2x.png"
+      />
+      <div class="">
+        <b
+          class="inline-block text-lg text-[#454548] mt-4 [text-shadow:0px_0px_0px_rgba(255,_255,_255,_0.01)]"
+        >
+          استخر بهمن تهران
+        </b>
+      </div>
+    </div>
+  </div>
+  <hr class="w-[70%] border border-gray-500 border-opacity-5 my-4 ml-40" />
+
+  <!-- all pool -->
+
+  <div
+    class="flex bg-[#f6f7fe] justify-center font-dana overflow-scroll my-20 w-full gap-4"
+  >
+    <div class="w-64 mr-[1100px] md:mr-[700px] lg:mr-80 2xl:mr-0 2xl:ml-10">
+      <p class="text-3xl mt-8 font-bold">
+        پارک های‌ <span class="text-[#1548FE]">آبــــی</span>
+      </p>
+      <p class="text-[#454548] font-normal text-xs">
+        پارک های آبی اطراف خود و در کل ایران همگی اینجا در یک لیست و با جزئیات
+        دقیق برای شما معرفی کند.
+      </p>
+      <img
+        src="./../../assets/slideicon.svg"
+        class="h-20 w-20 mt-16 mr-40"
+        alt=""
+      />
+    </div>
+
+    <div
+      class="flex flex-col items-center bg-white p-4 h-full shadow-2xl rounded-xl"
+    >
+      <img
+        class="rounded-xl w-64 h-[187px] object-cover"
+        alt=""
+        src="../../assets/image-3@2x.png"
+      />
+      <div class="">
+        <b
+          class="inline-block text-lg text-[#454548] mt-4 [text-shadow:0px_0px_0px_rgba(255,_255,_255,_0.01)]"
+          >پارک ساحلی آفتاب مشهد
+        </b>
+      </div>
+    </div>
+    <div
+      class="flex flex-col items-center bg-white p-4 h-full shadow-2xl rounded-xl"
+    >
+      <img
+        class="rounded-xl w-64 h-[187px] object-cover"
+        alt=""
+        src="../../assets/image-2@2x.png"
+      />
+      <div class="">
+        <b
+          class="inline-block mt-4 text-lg text-[#454548] [text-shadow:0px_0px_0px_rgba(255,_255,_255,_0.01)]"
+          >اُپارک تهران</b
+        >
+      </div>
+    </div>
+    <div
+      class="flex flex-col items-center bg-white p-4 h-full shadow-2xl rounded-xl"
+    >
+      <img
+        class="rounded-xl w-64 h-[187px] object-cover"
+        alt=""
+        src="../../assets/image-3@2x.png"
+      />
+      <div class="">
+        <b
+          class="inline-block text-lg mt-4 text-[#454548] [text-shadow:0px_0px_0px_rgba(255,_255,_255,_0.01)]"
+          >پارک ساحلی آفتاب مشهد
+        </b>
+      </div>
+    </div>
+    <div
+      class="flex flex-col items-center bg-white p-4 h-full shadow-2xl rounded-xl"
+    >
+      <img
+        class="rounded-xl w-64 h-[187px] object-cover"
+        alt=""
+        src="../../assets/image-3@2x.png"
+      />
+      <div class="">
+        <b
+          class="inline-block text-lg text-[#454548] mt-4 [text-shadow:0px_0px_0px_rgba(255,_255,_255,_0.01)]"
+          >پارک ساحلی آفتاب مشهد
+        </b>
+      </div>
+    </div>
+  </div>
+  <hr class="w-[70%] border border-gray-500 border-opacity-5 my-4 ml-40" />
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "newPool",
+});
+</script>
+<style lang=""></style>
---
+++ src/components/Home/Testimonial.vue
@@ -0,0 +1,94 @@
+<template lang="">
+  <!-- testimonial -->
+  <div
+    class="flex flex-col lg:flex-row justify-center bg-[#f6f7fe] text-center items-center font-dana my-10 w-full gap-24"
+  >
+    <div
+      class="flex w-96 h-full p-10 items-center flex-col bg-white shadow-2xl shadow-[#7591f8] rounded-xl"
+    >
+      <img
+        class="rounded-xl h-60 w-48 object-cover"
+        alt=""
+        src="../../assets/group-36866@2x.png"
+      />
+      <b
+        class="inline-block mt-4 text-2xl text-[#454548] [text-shadow:0px_0px_0px_rgba(255,_255,_255,_0.01)]"
+      >
+        ثبت نام <span class="text-[#1548FE]">ناجی</span> در آیپولز</b
+      >
+      <p class="text-[#454548] text-xs leading-5 mt-8">
+        دوره آموزشی مربیگری شنا مناسب برای کلیه علاقه مندان ، اساتید، مربیان و …
+        است.لورم ایپسوم متنی ساختگی است
+      </p>
+      <div class="flex items-center">
+        <p class="text-xs text-[#1548FE]">برای ثبت نام کلیک کنید</p>
+        <img
+          src="./../../assets/vuesaxlineararrowdown3.svg"
+          class="h-4 w-4"
+          alt=""
+        />
+      </div>
+    </div>
+    <div
+      class="flex w-96 h-full p-10 items-center flex-col bg-white shadow-2xl shadow-[#ffba81] rounded-xl"
+    >
+      <img
+        class="rounded-xl h-60 w-48 object-cover"
+        alt=""
+        src="../../assets/group-36860.svg"
+      />
+      <b
+        class="inline-block mt-4 text-2xl text-[#454548] [text-shadow:0px_0px_0px_rgba(255,_255,_255,_0.01)]"
+      >
+        ثبت نام <span class="text-[#FF6E04]">مربی</span> در آیپولز</b
+      >
+      <p class="text-[#454548] text-xs leading-5 mt-8">
+        دوره آموزشی مربیگری شنا مناسب برای کلیه علاقه مندان ، اساتید، مربیان و …
+        است.لورم ایپسوم متنی ساختگی است
+      </p>
+      <div class="flex items-center">
+        <p class="text-xs text-[#1548FE]">برای ثبت نام کلیک کنید</p>
+        <img
+          src="./../../assets/vuesaxlineararrowdown3.svg"
+          class="h-6 w-6"
+          alt=""
+        />
+      </div>
+    </div>
+
+    <div
+      class="flex w-96 h-full p-10 items-center flex-col bg-white shadow-2xl shadow-[#515059] rounded-xl"
+    >
+      <img
+        class="rounded-xl h-60 w-48 object-cover"
+        alt=""
+        src="../../assets/group-368601@2x.png"
+      />
+      <b
+        class="inline-block mt-4 text-2xl text-[#454548] [text-shadow:0px_0px_0px_rgba(255,_255,_255,_0.01)]"
+      >
+        همکاری با ما
+      </b>
+      <p class="text-[#454548] text-xs leading-5 mt-8">
+        دوره آموزشی مربیگری شنا مناسب برای کلیه علاقه مندان ، اساتید، مربیان و …
+        است.لورم ایپسوم متنی ساختگی است
+      </p>
+      <div class="flex items-center">
+        <p class="text-xs text-[#1548FE]">برای ثبت نام کلیک کنید</p>
+        <img
+          src="./../../assets/vuesaxlineararrowdown3.svg"
+          class="h-6 w-6"
+          alt=""
+        />
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "testimonial",
+});
+</script>
+<style lang=""></style>
